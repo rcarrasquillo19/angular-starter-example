b79dc59e6a46e4b1f4823c3aeb9df6e0acdd71c6
rcarrasquillo19 <r_carrasquillo@yahoo.com>
2020-11-07 23:27:38 -0500

    3 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Car } from './car';
 
 @Component({
   selector: 'my-app',
@@ -6,15 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 
-export class AppComponent  {
-
+export class AppComponent  
+{
   // variables declared here will be class variables
-  prefix = "I am a"
-  name = "Big Papi";
+  prefix = "I am a "
+  name = "Star Student";
 
-  speak() {
+  //Iterating through String Arrays
+  colors:string[] = ['red', 'blue', 'green', 'purple'];
+
+  //Iterating with Indices
+  fruits: string[] = ['apple', 'orange', 'pear', 'peach'];
+
+  //Iterating through Custom Types
+  subaru: Car = {make: 'Subaru', model: 'Outback', miles: 58232};
+  honda: Car = {make: 'Honda', model: 'Accord', miles: 39393};
+  bmw: Car = {make: 'BMW', model: 'X3', miles: 4400};
+  cars:Car[] = [this.subaru, this.honda, this.bmw];
+
+  speak() 
+  {
     //variables declared here will be method variables
     const sentence = this.prefix + this.name;
     return sentence;
   }
 }
+ 
